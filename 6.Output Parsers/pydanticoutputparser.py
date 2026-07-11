10cb660069c6c07718aed057083689b9b3ac246d
polarpeeps <ali31hasan@gmail.com>
2026-07-11 22:55:56 +0530

    Pydabtic Prompt for parser

--- 6.Output Parsers/pydanticoutputparser.py
+++ 6.Output Parsers/pydanticoutputparser.py
@@ -22,10 +22,17 @@ template = PromptTemplate(
     partial_variables={'format_instruction':parser.get_format_instructions()}
 )
 
+prompt = template.invoke({'place':'Luxembourg'})
+print(prompt)
+"""
+text='Generate the name, age and city of a fictional Luxembourg person \n The output should be formatted as a JSON instance that conforms to the JSON schema below.\n\nAs an example, for the schema {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}\nthe object {"foo": ["bar", "baz"]} is a well-formatted instance of the schema. The object {"properties": {"foo": ["bar", "baz"]}} is not well-formatted.\n\nHere is the output schema:\n```\n{"properties": {"name": {"description": "Name of the person", "title": "Name", "type": "string"}, "age": {"description": "Age of the person", "exclusiveMinimum": 18, "title": "Age", "type": "integer"}, "city": {"description": "Name of the city the person belongs to", "title": "City", "type": "string"}}, 
+"required": ["name", "age", "city"]}\n```'"""
 chain = template | model | parser
 
-final_result = chain.invoke({'place':'sri lankan'})
+final_result = chain.invoke({'place':'Luxembourg'})
 
 print(final_result)
 
-"""name='Samantha Silva' age=28 city='Colombo'"""
+"""name='Samantha Silva' age=28 city='Colombo'
+name='Sophie Muller' age=25 city='Luxembourg'
+"""
